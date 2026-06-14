91337302d6b3cd3dd32ae4913fee2e8496b563ba
Keerthana <keerthuzara1307@gmail.com>
2026-06-14 19:34:55 +0530

    Add IfDemo program

---
+++ Day03/IfDemo.java
@@ -0,0 +1,9 @@
+public class IfDemo {
+    public static void main(String[] args) {
+        int age = 20;
+
+        if (age >= 18) {
+            System.out.println("Eligible to Vote");
+        }
+    }
+}
