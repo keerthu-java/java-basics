9373e074d0ab510096ada2dd9e2045312dd5f660
Keerthana <keerthuzara1307@gmail.com>
2026-06-14 19:56:08 +0530

    Create ArrayDemo.java

---
+++ Day05/ArrayDemo.java
@@ -0,0 +1,10 @@
+public class ArrayDemo {
+    public static void main(String[] args) {
+
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        System.out.println(numbers[0]);
+        System.out.println(numbers[1]);
+        System.out.println(numbers[2]);
+    }
+}
